978ecb89d909d28a487332968d47457ffd12ecdd
jesuseladiom <jesuseladiom@gmail.com>
2025-02-19 12:18:09 -0700

    Limpiar cadenas

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -53,4 +53,54 @@
 
         }
 
+        /* ------ funcion para limpiar cadenas ----- */
+        protected static function limpiar_cadena($cadena) {
+            $cadena= trim($cadena);
+            // elimina las barras invertidas
+            $cadena= stripcslashes($cadena); 
+            // elimina el codigo script para evitar ataques maliciosos
+            $cadena= str_ireplace("<script>","",$cadena);
+            $cadena= str_ireplace("</script>","",$cadena);
+            $cadena= str_ireplace("<script src","",$cadena);
+            $cadena= str_ireplace("<script type","",$cadena);
+            $cadena= str_ireplace("SELECT * FROM","",$cadena);
+            $cadena= str_ireplace("DELETE FROM","",$cadena);
+            $cadena= str_ireplace("INSERT INTO","",$cadena);
+            $cadena= str_ireplace("DROP TABLE","",$cadena);
+            $cadena= str_ireplace("DROP DATABASE","",$cadena);
+            $cadena= str_ireplace("TRUNCATE TABLE","",$cadena);
+            $cadena= str_ireplace("SHOW TABLES","",$cadena);
+            $cadena= str_ireplace("SHOW DATABASES","",$cadena);
+            $cadena= str_ireplace("--","",$cadena);
+            $cadena= str_ireplace(">","",$cadena);
+            $cadena= str_ireplace("<","",$cadena);
+            $cadena= str_ireplace("[","",$cadena);
+            $cadena= str_ireplace("]","",$cadena);
+            $cadena= str_ireplace("^","",$cadena);
+            $cadena= str_ireplace("?>","",$cadena);
+            $cadena= str_ireplace("==","",$cadena);
+            $cadena= str_ireplace(";","",$cadena);
+            $cadena= str_ireplace("::","",$cadena);
+
+            return $cadena;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
     }
